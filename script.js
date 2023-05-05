b565320a2c5a8c12f71d8f01a9404fcc35510f75
Robert Reigstad <robrei1robert@gmail.com>
2023-05-05 09:19:02 +0200

    Link script.js to HTML and finalize RPS logic

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
 // RPS Game
 
 //- The player will get a prompt asking for 'Rock', 'Paper' or 'Scissors'.
-const playerSelection = prompt('Type Rock, Paper or Scissors: ');
+const playerChoice = prompt('Type Rock, Paper or Scissors: ').toLowerCase();
 //- A function randomly picks 'Rock', 'Paper' or 'Scissors' for the computer and saves it to a variable.
 function getComputerChoice() {
     let random = Math.floor((Math.random() * 3));
     let choice = ['Rock', 'Paper', 'Scissors'];
-    return choice[random];
+    return choice[random].toLowerCase();
 }
 
-const computerSelection = getComputerChoice();
-//- Add RPS logic to a 'playGame' function that takes playerSelection and computerSelection as params.
+const computerChoice = getComputerChoice();
+//- Add RPS logic to a 'playGame' function that takes playerChoice and computerSelection as params.
 function playGame(playerSelection, computerSelection) {
-
+    if(playerSelection === 'rock' && computerSelection === 'rock') {
+        console.log('Its a tie!');
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        console.log('You loose! Paper beats Rock!');
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        console.log('You win! Rock beats Scissors!');
+    } else if (playerSelection === 'paper' && computerSelection === 'paper') {
+        console.log('Its a tie!');
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        console.log('You win! Paper beats Rock!');
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        console.log('You loose! Scissors beats Paper!');
+    } else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
+        console.log('Its a tie!');
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        console.log('You win! Scissors beats Paper!');
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        console.log('You loose! Rock beats Scissors!');
+    } else {
+        console.log('Something went wrong..');
+    }
 }
 //- Call function and display it in console.
-
-
+console.log(playGame(playerChoice, computerChoice));
